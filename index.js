af983dadf38eb794344e0b2d95bdde812d7df17d
alan <cvinny33@gmail.com>
2022-08-26 18:01:19 -0600

    finished constructors & added prompts in index.js

--- index.js
+++ index.js
@@ -1,3 +1,117 @@
-const Engineer = require("./lib/Manager")
+const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
-const Engineer = require("./lib/Intern")
+const Intern = require("./lib/Intern")
+const inquirer = require("inquirer")
+const team = []
+const promptEngineer = () => {
+inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message:'What is the Engineers name',
+   
+},
+{
+    type: 'input',
+    name: 'id',
+    message:'What is the Engineers ID',
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is the Engineers email',
+},
+{
+    type: 'input',
+    name: 'github',
+    message:'What is the Engineers github',
+}
+])  
+.then((data) => {
+    const member = new Engineer(data.name, data.id, data.email, data.github)
+    team.push(member)
+    menu()
+}) 
+}
+const promptIntern = () => {
+inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message:'What is the Intern name',
+   
+},
+{
+    type: 'input',
+    name: 'id',
+    message:'What is the Intern ID',
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is the Intern email',
+},
+{
+    type: 'input',
+    name: 'school',
+    message:'What is the Intern school',
+}
+])  
+.then((data) => {
+    const member = new Intern(data.name, data.id, data.email, data.school)
+    team.push(member)
+    menu()
+}) 
+}
+const promptManager = () => {
+inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message:'What is the Manager name',
+   
+},
+{
+    type: 'input',
+    name: 'id',
+    message:'What is the Manager ID',
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is the Manager email',
+},
+{
+    type: 'input',
+    name: 'officeNumber',
+    message:'What is the Manager officeNumber',
+}
+])  
+.then((data) => {
+    const member = new Manager(data.name, data.id, data.email, data.officeNumber)
+    team.push(member)
+    menu()
+}) 
+}
+
+const menu = () => {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'team member create',
+        choices: ['Manager', 'Engineer', 'Intern', 'Done']
+    }])
+    .then((data) => {
+        switch(data.action){
+            case 'Manager':
+                promptManager()
+                break;
+            case 'Engineer':
+                promptEngineer()
+                break;
+            case 'Intern':
+                promptIntern()
+                break;
+            default:
+                console.log(team) 
+        }
+    })
+}
+menu()
