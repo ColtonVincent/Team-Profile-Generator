d7aebb523205a04825df9a26a69005aa966eabac
alan <cvinny33@gmail.com>
2022-09-06 22:46:42 -0600

    added styling and those methods to generateHtml

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -2,22 +2,16 @@ const generateHTML = (data) => {
   const genManager = (managers) => {
     const html = []
     managers.forEach(manager => {
-      html.push(`<div>
-      <p>${manager.name}</p>
-      <p>${manager.id}</p>
-      <p>${manager.email}</p>
-      <p>${manager.officeNumber}</p>
-      </div>`)
       html.push(` <div class="card">
       <div class="card-header">
           <h2 class="card-title">${manager.name}</h2>
-          <h3 class="card-title"> Manager</h3>
+          <h3 class="card-title">Manager</h3>
       </div>
       <div class="card-body">
           <ul>
-              <li class="body-list">home</li>
-              <li class="body-list">body</li>
-              <li class="body-list">main</li>
+              <li class="body-list">ID: ${manager.id}</li>
+              <li class="body-list">Email: ${manager.email}</li>
+              <li class="body-list">Office number: ${manager.officeNumber}</li>
           </ul>
       </div>
   </div>`)
@@ -27,24 +21,38 @@ const generateHTML = (data) => {
   const genIntern = (interns) => {
     const html = []
     interns.forEach(intern => {
-      html.push(`<div>
-      <p>${intern.name}</p>
-      <p>${intern.id}</p>
-      <p>${intern.email}</p>
-      <p>${intern.school}</p>
-      </div>`)
+      html.push(` <div class="card">
+      <div class="card-header">
+          <h2 class="card-title">${intern.name}</h2>
+          <h3 class="card-title">Intern</h3>
+      </div>
+      <div class="card-body">
+          <ul>
+              <li class="body-list">ID: ${intern.id}</li>
+              <li class="body-list">Email: ${intern.email}</li>
+              <li class="body-list">School: ${intern.school}</li>
+          </ul>
+      </div>
+  </div>`)
     }); 
     return html.join('')
   }
   const genEngineer = (engineers) => {
     const html = []
     engineers.forEach(engineer => {
-      html.push(`<div>
-      <p>${engineer.name}</p>
-      <p>${engineer.id}</p>
-      <p>${engineer.email}</p>
-      <p>${engineer.github}</p>
-      </div>`)
+      html.push(` <div class="card">
+      <div class="card-header">
+          <h2 class="card-title">${engineer.name}</h2>
+          <h3 class="card-title">Engineer</h3>
+      </div>
+      <div class="card-body">
+          <ul>
+              <li class="body-list">ID: ${engineer.id}</li>
+              <li class="body-list">Email: ${engineer.email}</li>
+              <li class="body-list">Github: ${engineer.github}</li>
+          </ul>
+      </div>
+  </div>`)
     }); 
     return html.join('')
   }
@@ -55,20 +63,23 @@ const generateHTML = (data) => {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" type="text/css" href="./style.css" />
-      <title>Team Profile Generator</title>
+      <link rel="stylesheet" href="../dist/style.css">
+      <title>Document</title>
   </head>
   <body>
-    ${genManager(data.filter(manager => manager.getRole() === 'Manager'))}
-    ${genIntern(data.filter(intern => intern.getRole() === 'Intern'))}
-    ${genEngineer(data.filter(engineer => engineer.getRole() === 'Engineer'))}
+      <header>
+          <nav>My Team</nav>
+      </header>
+  <div class="card-wrapper">
+  ${genManager(data.filter(manager => manager.getRole() === 'Manager'))}
+  ${genIntern(data.filter(intern => intern.getRole() === 'Intern'))}
+  ${genEngineer(data.filter(engineer => engineer.getRole() === 'Engineer'))}
+  </div>
   </body>
-  </html>`
+  </html>
+  `
 }
 module.exports = generateHTML
-
-
-
 
 
 
